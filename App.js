35d5ca1e12bfdbe6709d7680d0a01ac2c6ca8f0b
Md Zikrullah <mdzikrullahaug2001@gmail.com>
2025-05-12 09:01:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $("#searchBtn").click(function(){
+        let city = $("#textBox").val();
+        $("#textBox").val("");
+        let key = "a358295a53dd3ba2b55af692864a5c5d";
+        if(city != ""){
+            $.ajax({
+                url: "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+key+"&units=metric" ,
+                method: "GET",
+                dataType: "json",
+                timeout: 3000,
+
+                success: function(data){
+                    console.log(data);
+                    $(".temp").html(data.main.temp+" <sup>o</sup>C");
+                    $(".temp").text(data.name);
+                    $(".hmdt").text(data.main.humidity+" %");
+                    $(".wind1").html(data.wind.speed+" km/h");
+                },
+                error: function(xhr,status,err){
+                    if(status === "timeout"){
+                        alert("Request Time Out.");
+                    }else{
+                        console.log(err);
+                    }
+                }
+            });
+            
+        }else{
+            alert("Search box can't be empty");
+        }
+    });
+});
